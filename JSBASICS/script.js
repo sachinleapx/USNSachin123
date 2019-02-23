5c38f415c9b3cacd7a084d503863d9b148a81d65
Sachin Srivastava <sachin@leapxlabs.com>
2019-02-23 16:50:48 +0530

    added JS basics content

---
+++ JSBASICS/script.js
@@ -0,0 +1,57 @@
+/*
+//decision making
+var age=18;
+if (age<18){
+    console.log("minor");
+}else if (age >= 18){
+    console.log("major");
+}
+
+//con?true:false
+age>=18?console.log("major"):console.log("minor");
+
+//function
+function sachin(firstName,Lastname){
+    console.log(firstName+' '+Lastname);
+}
+sachin('Deepti','Agarwal');
+
+//array
+
+var names=['sachin','Ankita','Avyaan'];
+console.log(names[2]);
+console.log(names.length);
+
+names.push("Shrihaan");
+names.unshift("Rajesh");
+names.pop();
+console.log(names);
+
+//Objects
+
+var myDetails = {
+    firstName: 'Sachin',
+    lastName: 'Srivastava',
+    doB: 1980,
+    family: ['sachin','ankita'],
+    calcAge: function(birthYear){
+        return 2019-birthYear;
+    }
+};
+
+console.log(myDetails.firstName);
+var z='doB';
+console.log(myDetails[z]);
+
+myDetails.doB=1981;
+console.log(myDetails);
+
+//new object
+
+var shiva = new Object();
+shiva.firstName='Deepti';
+console.log(shiva);
+
+//
+console.log('Sachin is ' +myDetails.calcAge(1980)+' years old');
+*/
